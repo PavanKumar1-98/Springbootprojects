3a37797d2a63c6de8e40b881d4994baa16ee55c8
Pavan <Pavankumarpulavarthy1@gmail.com>
2025-04-01 12:21:52 +0530

    Added single file to project

---
+++ demo/src/main/java/com/TestProject/demo/cardinalities/ManytoManyMappings/Test.java
@@ -0,0 +1,7 @@
+package com.TestProject.demo.cardinalities.ManytoManyMappings;
+
+public class Test {
+   public static void main(String[] args){
+       System.out.println("pavan kumar pulavarthi");
+   }
+}
